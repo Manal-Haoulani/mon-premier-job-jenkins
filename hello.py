20e393fd587d9ddba21adde0b0481ae8831b4315
manal haoulani <miyamihyt@gmail.com>
2026-05-19 12:05:13 +0100

    Ajouter le script

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("=" *40)
+print("Bienvenue dan mon premier job jenkins !")
+print("=" *40)
+
+nom = input("Quel est ton nom ?")
+print(f"Bonjour {nom}, ton job Jenkins a réussi !")
+ 
+a = 10
+b = 5
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
